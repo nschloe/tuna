397b349ab1a9bd75cda16dc9000fdeccaf08b01f
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-22 17:50:51 +0200

    serve from other ports

--- tuna/main.py
+++ tuna/main.py
@@ -8,6 +8,7 @@ import pstats
 import socketserver
 import tempfile
 import threading
+import time
 import webbrowser
 
 
@@ -58,23 +59,38 @@ def read(prof_filename):
 
 
 class ServerThread(threading.Thread):
-    def __init__(self, web_dir, port):
+    def __init__(self, web_dir):
         threading.Thread.__init__(self)
         self.web_dir = web_dir
-        self.port = port
+        self.port = None
         return
 
     def run(self):
         os.chdir(self.web_dir)
         Handler = http.server.SimpleHTTPRequestHandler
-        httpd = socketserver.TCPServer(("", self.port), Handler)
-        print("serving at port", self.port)
+        # Sometimes, port 8000 is occupied. Just keep trying then.
+        self.port = None
+        for port in [8000 + k for k in range(100)]:
+            try:
+                httpd = socketserver.TCPServer(("", port), Handler)
+            except OSError:
+                print("Port {} in use, trying another.".format(port))
+            else:
+                self.port = port
+                break
+
+        assert self.port is not None, "Could not find open port."
+
+        print("serving at port {}".format(self.port))
         httpd.serve_forever()
         return
 
 
 def start_server():
-    web_dir = os.path.join(os.path.dirname(__file__), 'web')
-    ServerThread(web_dir, 8080).start()
-    webbrowser.open_new_tab("http://localhost:8080")
+    web_dir = os.path.join(os.path.dirname(__file__), "web")
+    thread = ServerThread(web_dir)
+    thread.start()
+    while thread.port is None:
+        time.sleep(0.1)
+    webbrowser.open_new_tab("http://localhost:{}".format(thread.port))
     return
