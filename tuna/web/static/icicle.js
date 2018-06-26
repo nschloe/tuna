4689b8032e07bdc5d85019a072ddf34ad75df17b
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-26 19:47:22 +0200

    get basic zooming to work

--- tuna/web/static/icicle.js
+++ tuna/web/static/icicle.js
@@ -4,6 +4,7 @@ class Icicle extends HTMLElement {
     this.data = JSON.parse(this.getAttribute('data'));
     this.width = this.getAttribute('width');
     this.height = this.getAttribute('height');
+    this.myheight = this.getAttribute('height');
     this.render();
   }
 
@@ -15,10 +16,17 @@ class Icicle extends HTMLElement {
     // zoom
     // unzoom
     // some interesting test cases
+    // favicon
 
-    const partition = d3.partition()
-      .size([this.width, this.height])
-      .round(true);
+    var x = d3.scaleLinear()
+      .range([0, this.width]);
+
+    var y = d3.scaleLinear()
+      .range([0, this.height]);
+
+    const partition = d3.partition();
+    // .size([this.width, this.height])
+    // .round(true);
 
     const svg = d3.select("body").append("svg")
       .attr("width", this.width)
@@ -36,10 +44,10 @@ class Icicle extends HTMLElement {
       .enter().append("g");
 
     const rect = g.append("rect")
-      .attr("x", function(d) { return d.x0; })
-      .attr("y", function(d) { return d.y0; })
-      .attr("width", function(d) { return d.x1 - d.x0; })
-      .attr("height", function(d) { return d.y1 - d.y0; })
+      .attr("x", function(d) { return x(d.x0); })
+      .attr("y", function(d) { return y(d.y0); })
+      .attr("width", function(d) { return x(d.x1 - d.x0); })
+      .attr("height", function(d) { return y(d.y1 - d.y0); })
       .attr("fill", function(d) { return color((d.children ? d : d.parent).key); })
       .on("click", clicked);
 
@@ -49,40 +57,42 @@ class Icicle extends HTMLElement {
 
     // Now add the text. First, the clip path.
     const clipPath = g.append("clipPath")
-      .attr("id", function(d) { return "cp_" + d.x0 + "_" + d.x1 + "_" + d.y0 + "_" + d.y1; })
+      .attr("id", function(d) { return "cp_" + Math.round(x(d.x0)) + "_" + Math.round(x(d.x1)) + "_" + Math.round(y(d.y0)) + "_" + Math.round(y(d.y1)); })
     clipPath.append("rect")
-      .attr("x", function(d) { return d.x0; })
-      .attr("y", function(d) { return d.y0; })
-      .attr("width", function(d) { return d.x1 - d.x0; })
-      .attr("height", function(d) { return d.y1 - d.y0; });
+      .attr("x", function(d) { return x(d.x0); })
+      .attr("y", function(d) { return y(d.y0); })
+      .attr("width", function(d) { return x(d.x1) - x(d.x0); })
+      .attr("height", function(d) { return y(d.y1) - y(d.y0); });
     // Now the text. Multiline text is realized with <tspan> in SVG.
     const text = g.append("text")
-      .attr("y", function(d) { return (d.y0 + d.y1)/2; })
+      .attr("y", function(d) { return y(d.y0 + d.y1)/2; })
       .attr("alignment-baseline", "middle")
       .attr("text-anchor", "middle")
       .attr("fill", "white")
-      .attr("clip-path", function(d) { return "url(#" + "cp_" + d.x0 + "_" + d.x1 + "_" + d.y0 + "_" + d.y1 + ")"; });
+      .attr("clip-path", function(d) { return "url(#" + "cp_" + Math.round(x(d.x0)) + "_" + Math.round(x(d.x1)) + "_" + Math.round(y(d.y0)) + "_" + Math.round(y(d.y1)) + ")"; });
     text.append("tspan")
       .text(function(d) { return d.data.name })
       .attr("font-family", "sans-serif")
-      .attr("x", function(d) { return (d.x0 + d.x1)/2; });
+      .attr("x", function(d) { return x(d.x0 + d.x1)/2; });
     text.append("tspan")
       .text(function(d) { return d3.format(".3e")(d.value) + " s" })
       .attr("font-family", "sans-serif")
-      .attr("x", function(d) { return (d.x0 + d.x1)/2; })
+      .attr("x", function(d) { return x(d.x0 + d.x1)/2; })
       .attr("dy", "1.5em");
 
-    function clicked(d) {
-      console.log("clicked");
-      // x.domain([d.x0, d.x1]);
-      // y.domain([d.y0, 1]).range([d.y0 ? 20 : 0, height]);
+    // make height available in clicked()
+    const height = this.height;
 
-      // rect.transition()
-      //     .duration(750)
-      //     .attr("x", function(d) { return d.x0; })
-      //     .attr("y", function(d) { return d.y0; })
-      //     .attr("width", function(d) { return d.x1 - d.x0; })
-      //     .attr("height", function(d) { return d.y1 - d.y0; });
+    function clicked(d) {
+      x.domain([d.x0, d.x1]);
+      y.domain([d.y0, 1]).range([d.y0 ? 20 : 0, height]);
+
+      rect.transition()
+        .duration(750)
+        .attr("x", function(d) { return x(d.x0); })
+        .attr("y", function(d) { return y(d.y0); })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0); })
+        .attr("height", function(d) { return y(d.y1) - y(d.y0); });
     }
   }
 }
