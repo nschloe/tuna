3d359ab5dfc1a1c3e500e0209b093845a8590919
Nico Schlömer <nico.schloemer@gmail.com>
2019-02-16 19:37:26 +0100

    add license file
    Fixes #36.

--- tuna/__about__.py
+++ tuna/__about__.py
@@ -3,7 +3,7 @@
 
 __author__ = u"Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
-__copyright__ = u"Copyright (c) 2018, {} <{}>".format(__author__, __author_email__)
+__copyright__ = u"Copyright (c) 2018-2019, {} <{}>".format(__author__, __author_email__)
 __credits__ = []
 __license__ = "License :: OSI Approved :: MIT License"
 __version__ = "0.2.0"
