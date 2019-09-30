2807ff55e3eef6f938ed5f30d8cc91583ecf5a25
Nico Schlömer <nico.schloemer@gmail.com>
2019-09-30 15:55:14 +0200

    version bump

--- tuna/__about__.py
+++ tuna/__about__.py
@@ -2,6 +2,6 @@ __author__ = "Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __copyright__ = "Copyright (c) 2018-2019, {} <{}>".format(__author__, __author_email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __maintainer__ = "Nico Schlömer"
 __status__ = "Development Status :: 5 - Production/Stable"
