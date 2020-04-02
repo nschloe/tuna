eca5f46d28d3c5f827520dba59d1a873f89cd4e7
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-02 23:55:51 +0200

    modernize

--- tuna/__init__.py
+++ tuna/__init__.py
@@ -1,19 +1,9 @@
 from . import cli
-from .__about__ import (
-    __author__,
-    __author_email__,
-    __license__,
-    __status__,
-    __version__,
-)
+from .__about__ import __version__
 from .main import read_import_profile
 
 __all__ = [
-    "__author__",
-    "__author_email__",
-    "__license__",
     "__version__",
-    "__status__",
     "cli",
     "read_import_profile",
 ]
